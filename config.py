d4137c6911587786e1829aaff2212797454f6680
ABDERRAZZAK EL HAMCHIOUI <68821194+ELHAMCHIOUI@users.noreply.github.com>
2026-05-01 12:53:03 +0000

    first commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = "YOUR_TELEGRAM_TOKEN"
+TOKEN = "8618872090:AAHIM-6ibsb-yBnTE_KDRDa2UzZ3kC3_GYE"
 NOWPAYMENTS_API_KEY = "YOUR_API_KEY"
 
 PAYPAL_LINK = "https://paypal.me/yourname/5"
-REVOLUT_LINK = "https://revolut.me/yourname"
+REVOLUT_LINK = "https://revolut.me/aelhamchioui"
